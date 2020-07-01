3bd5218d7366bfac0ae328516d21722e22853462
Angelina Rubis <angelinarubis@Angelinas-MacBook-Pro.local>
2020-07-01 19:26:31 -0400

    Change background color to white

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ file_put_contents($name, $data);
     <link href="jsPsych/css/jspsych.css" rel="stylesheet" type="text/css"></link>
     <link rel="stylesheet" type="text/css" href="css/style.css">
   </head>
-  <body  style="background-color:grey;">  
+  <body  style="background-color:white;">  
     <?php include 'include/consent.php'?>
   </body>
   <footer>
